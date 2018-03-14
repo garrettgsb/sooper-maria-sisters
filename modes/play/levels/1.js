dbbc7c088dd3ede2ea7fa0c47c43ce08283cc3e6
Garrett <garrettgsb@gmail.com>
2018-03-14 00:18:01 -0700

    Extreme improvement of the Level1 constructor.  🍐🍐🍐
    This commit was 'a mistake, carried through to perfection' -- Edsgar Dijkstra

--- modes/play/levels/1.js
+++ modes/play/levels/1.js
@@ -4,21 +4,20 @@ class Level1 {
     this.mode = mode;
     this.ground = (() => {
       const arr = [];
-      for (var i=2; i<300; i++) {
-        if (i % 25 > 0) {
-          arr.push(new GroundTile(i*20, 300))
-        }
-
-        if (!(i % 14 > 0)) {
-          arr.push(new Trampoline(i*20,240));
-        }
-
-        if (!(i % 8 > 0)) {
-          arr.push(new GroundTile(i*20,140));
+      var alignments = [
+        [25, GroundTile, 300, true],
+        [14, Trampoline, 240],
+        [8, GroundTile, 140],
+      ]
+      for (var i = 2; i < 300; i++) {
+        for (var [modulus, ObjectClass, height, invert] of alignments) {
+          if (( invert && (i % modulus === 0)) || (!invert && (i % modulus !== 0))) {
+            arr.push(new ObjectClass(i * 20, height));
+          }
         }
       }
       return arr;
-    })()
+    })();
   }
 
   get mobs() {
